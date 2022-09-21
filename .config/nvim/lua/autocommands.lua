b9f666b40a6f87170a10e6bee6246e1950444cb9
Konrad Nölle <konrad@familie-noelle.de>
2022-09-21 17:52:08 +0200

    nvim: Fix entering insert mode on open terminal

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -4,5 +4,8 @@ augroup remember_cursor_position
     autocmd!
     autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 augroup END
-autocmd BufEnter * if &buftype == 'terminal' | :startinsert | endif
+
+augroup auto_insert_terminal
+    autocmd TermOpen * :startinsert
+augroup END
 ]])
